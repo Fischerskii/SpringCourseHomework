7e4df2f78be73a61944998d1242f4d233a702f66
Pavel Trofimov <r-half@yandex.ru>
2021-11-21 19:20:30 +0300

    add data to HashMap

--- src/main/java/ru/springcourse/homeworks/homeworks/firstHomeWrok/Main.java
+++ src/main/java/ru/springcourse/homeworks/homeworks/firstHomeWrok/Main.java
@@ -3,17 +3,22 @@ package ru.springcourse.homeworks.homeworks.firstHomeWrok;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import ru.springcourse.homeworks.homeworks.firstHomeWrok.impl.ExternalServiceImpl;
 
 @ComponentScan
 @PropertySource("classpath:application.properties")
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Main.class);
+        ExternalServiceImpl externalService = applicationContext.getBean(ExternalServiceImpl.class);
 
         Flow flow = applicationContext.getBean(Flow.class);
         flow.run(1);
         flow.run(2);
         flow.run(3);
         flow.run(4);
+
+        externalService.cleaner();
+        applicationContext.close();
     }
 }
--- src/main/java/ru/springcourse/homeworks/homeworks/firstHomeWrok/impl/ExternalServiceImpl.java
+++ src/main/java/ru/springcourse/homeworks/homeworks/firstHomeWrok/impl/ExternalServiceImpl.java
@@ -7,18 +7,32 @@ import ru.springcourse.homeworks.homeworks.firstHomeWrok.CacheResult;
 import ru.springcourse.homeworks.homeworks.firstHomeWrok.ExternalInfo;
 import ru.springcourse.homeworks.homeworks.firstHomeWrok.ExternalService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component("externalServiceImpl")
 public class ExternalServiceImpl implements ExternalService {
-    Map<Integer, ExternalInfo> externalInfoMap;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(ExternalServiceImpl.class);
+
+    Map<Integer, ExternalInfo> externalInfoMap = new HashMap<>();
+
+    public ExternalServiceImpl() {
+        this.externalInfoMap.put(1, new ExternalInfo(1, null));
+        this.externalInfoMap.put(2, new ExternalInfo(2, "hashInfo"));
+        this.externalInfoMap.put(3, new ExternalInfo(3, "info"));
+        this.externalInfoMap.put(4, new ExternalInfo(4, "information"));
+    }
 
     @CacheResult
     @Override
     public ExternalInfo getExternalInfo(Integer id) {
-        LOGGER.info(String.valueOf(externalInfoMap));
+        LOGGER.info("Method getExternalInfo called");
         return externalInfoMap.get(id);
+    }
+
+    public void cleaner() {
+        this.externalInfoMap.clear();
+
+        LOGGER.info("HashMap cleared");
     }
 }
