0c6a2a04a608611bae0fc09cb4776500fcede5bb
Pavel Trofimov <r-half@yandex.ru>
2021-11-21 21:22:33 +0300

    removed inactive lines

--- src/main/java/ru/springcourse/homeworks/homeworks/firstHomeWrok/Flow.java
+++ src/main/java/ru/springcourse/homeworks/homeworks/firstHomeWrok/Flow.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class Flow {
 
