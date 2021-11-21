cebe5798ad5175969c88f0029bf60e9cef74f8d6
Pavel Trofimov <r-half@yandex.ru>
2021-11-21 23:36:53 +0300

    add verify beans for contains annotate @acheResult and prototype beans

--- src/main/java/ru/springcourse/homeworks/externalServiceApplication/CacheResult.java
+++ src/main/java/ru/springcourse/homeworks/externalServiceApplication/CacheResult.java
@@ -1,13 +1,10 @@
 package ru.springcourse.homeworks.externalServiceApplication;
 
-import org.springframework.cache.annotation.Cacheable;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Cacheable(value = "id")
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CacheResult {
--- src/main/java/ru/springcourse/homeworks/externalServiceApplication/PrintBeanFactoryPostProcessor.java
+++ src/main/java/ru/springcourse/homeworks/externalServiceApplication/PrintBeanFactoryPostProcessor.java
@@ -5,16 +5,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class PrintBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
     private static final Logger LOGGER = LoggerFactory.getLogger(PrintBeanFactoryPostProcessor.class);
+    ApplicationContext applicationContext;
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        Map<String, Object> beansWithAnnotation = configurableListableBeanFactory.getBeansWithAnnotation(CacheResult.class);
+        if (!beansWithAnnotation.isEmpty()) {
+            LOGGER.info("Have beans containing annotation @CacheResult");
+            for (Map.Entry<String, Object> map : beansWithAnnotation.entrySet()) {
+                System.out.println("Вывод мапы " + map);
+            }
+        }
+
         String[] beanDefinitionNames = configurableListableBeanFactory.getBeanDefinitionNames();
-        for(String beanDefinitionName : beanDefinitionNames) {
+        for (String beanDefinitionName : beanDefinitionNames) {
+            if (configurableListableBeanFactory.isPrototype(beanDefinitionName)) {
+                LOGGER.warn(beanDefinitionName + " - isPrototype");
+            }
             LOGGER.info("BeanDefinitionName " + beanDefinitionName);
         }
     }
